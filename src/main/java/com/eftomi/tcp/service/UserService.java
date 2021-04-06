10942320b5ee77fededf3a286b4d28cf0b4e6909
Peter Nagy <peter.nagy@perit.hu>
2021-04-06 12:27:38 +0200

    pessimistic locking

--- src/main/java/com/eftomi/tcp/service/UserService.java
+++ src/main/java/com/eftomi/tcp/service/UserService.java
@@ -2,7 +2,10 @@ package com.eftomi.tcp.service;
 
 import java.util.Optional;
 
+import javax.persistence.LockModeType;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,11 +20,12 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userDAO;
-	
+
 	@Transactional
+	@Lock(value = LockModeType.PESSIMISTIC_WRITE)
 	public boolean registration(String name, String email, String password) {
 		log.debug(String.format("registration('%s')", email));
-		
+
 		Optional<User> optUser = userDAO.findByEmail(email);
 		if (optUser.isEmpty() && valid(name, email, password)) {
 			userDAO.save(new User(name, email, password));
@@ -30,10 +34,10 @@ public class UserService {
 			return false;
 		}
 	}
-	
+
 	public boolean login(String email, String password) {
 		log.debug(String.format("login('%s')", email));
-		
+
 		Optional<User> optUser = userDAO.findByEmailAndPassword(email, password);
 		return optUser.isPresent();
 	}
@@ -41,12 +45,13 @@ public class UserService {
 	public Optional<User> getUser(String email) {
 		return userDAO.findByEmail(email);
 	}
-	
+
 	private boolean valid(String name, String email, String password) {
-		if (email.matches("[a-zA-Z0-9.@_]+") && email.contains("@") && email.contains(".") && password.length() > 2 && name.length() > 2) {
+		if (email.matches("[a-zA-Z0-9.@_]+") && email.contains("@") && email.contains(".") && password.length() > 2
+				&& name.length() > 2) {
 			return true;
 		} else {
-			return false;  //Regisztráció ellenőrzés kikapcsolása: true.
+			return false; // Regisztráció ellenőrzés kikapcsolása: true.
 		}
 	}
 }
